20ba87405fd00dbc76a309f06744b628f0cf0f3b
Rodolfo Mariano <rodolfomarianocy@gmail.com>
2022-07-15 14:17:59 -0300

    Update hackshell.py

--- hackshell.py
+++ hackshell.py
@@ -214,7 +214,7 @@ try:
     elif sys.argv[1] == "2a":
         print("Builtin's RCE - Filter Bypass:")
         privesc()
-    elif sys.argv[1] == "--help" or "help":
+    elif sys.argv[1] == "--help":
         menu()
 except:
     menu()
